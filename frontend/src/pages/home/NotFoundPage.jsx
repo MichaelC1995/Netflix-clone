3fcdcb6647f927a3eb00237d75dcd8bcfa69e9c4
MichaelC1995 <m.chatellier@yahoo.com>
2025-01-12 20:46:17 -0600

    notfoundpage fix

--- frontend/src/pages/home/NotFoundPage.jsx
+++ frontend/src/pages/home/NotFoundPage.jsx
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
     return (
 
         <div
-            className='min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white'
+            className='min-h-screen bg-black overflow-hidden h-full bg-cover bg-center flex flex-col justify-center items-center text-white'
             style={{backgroundImage: `url('/404.png')`}}
         >
             <header className='absolute top-0 left-0 bg-black/70 w-full '>
